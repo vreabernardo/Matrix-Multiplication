8979f5e745a6cbf37cdf0331d4025eef104f7631
Bernardo Martins <vreabernardo@gmail.com>
2024-05-05 13:20:18 +0100

    Update mm.py

--- mm.py
+++ mm.py
@@ -6,12 +6,12 @@ N = 4090
 if __name__ == "__main__":
     A = np.random.randn(N, N).astype(np.float32)
     B = np.random.randn(N, N).astype(np.float32)
-    
+
+    # N^2 output cells with 2N compute each
     flop = N * N * 2 * N
     
     st = time.monotonic()
     
-    # N^2 output cells with 2N compute each
     C = A @ B
 
     et = time.monotonic()
