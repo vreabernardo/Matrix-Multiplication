81420d5e88d3dd1a7a15289c430c658fed2aa332
vreabernardo@ubuntu <vreabernardo@ubuntu>
2024-05-05 11:41:09 +0100

    test with identity matrices to check if the result is correct

--- mm.c
+++ mm.c
@@ -1,55 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 #define N 5
 
-float A[N][N];
-float B[N][N];
-float S[N][N];
-
-
 int main() {
-   
+    float A[N][N];
+    float B[N][N];
     int i, j;
-    srand(time(NULL));
-       
-    //populate A matrix
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            // Generate a random float between 0.0 and 1.0
-            A[i][j] = (float)rand() / (float)RAND_MAX;
-        }
-    
-    }
-    
-    // populate B matrix
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            // Generate a random float between 0.0 and 1.0
-            B[i][j] = (float)rand() / (float)RAND_MAX;
-        }
-    }
- 
 
-        // Print the matrix
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
-            printf("%.2f\t", A[i][j]); // Print with two decimal precision
+            A[i][j] = (i == j) ? 1.0 : 0.0;
+        }
+    }
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            B[i][j] = (i == j) ? 1.0 : 0.0;
+        }
+    }
+
+    printf("Matrix A:\n");
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            printf("%.1f ", A[i][j]);
         }
         printf("\n");
     }
-    
-    printf("\n");
 
+    printf("Matrix B:\n");
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
-            printf("%.2f\t", B[i][j]); // Print with two decimal precision
+            printf("%.1f ", B[i][j]);
         }
         printf("\n");
-    } 
-    printf("\n");
-
+    }
 
     return 0;
 }
