566037fb110f57479c76b223c3804f004c1b0ef2
vreabernardo@ubuntu <vreabernardo@ubuntu>
2024-05-05 09:53:34 +0100

    mm in python3

---
+++ mm.py
@@ -0,0 +1,22 @@
+import numpy as np
+import time
+
+N = 4096
+
+if __name__ == "__main__":
+    A = np.random.randn(N, N).astype(np.float32)
+    B = np.random.randn(N, N).astype(np.float32)
+    
+    flop = N * N * 2 * N
+    
+    st = time.monotonic()
+    
+    # N^2 output cells with 2N compute each
+    C = A @ B
+
+    et = time.monotonic()
+    
+    s=et-st
+
+    print(f"{flop/s * 1e-9:.2f} GFLOPS")
+    
